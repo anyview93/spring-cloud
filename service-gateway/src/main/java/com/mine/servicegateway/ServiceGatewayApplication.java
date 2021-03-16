ee1228ec0497c85300b8724cf470614709f29e80
shizhiguo <shizhiguo@thunisoft.com>
2021-03-16 18:27:59 +0800

    提交

---
+++ service-gateway/src/main/java/com/mine/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,77 @@
+package com.mine.servicegateway;
+
+import com.mine.servicegateway.filter.RequestTimeFilter;
+import com.mine.servicegateway.filter.RequestTimeGatewayFilterFactory;
+import com.mine.servicegateway.filter.TokenFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class ServiceGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/customer/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
+        return builder.routes()
+                /*.route(r -> r.path("/customer/**")
+                        .filters(f -> f.filter(new RequestTimeFilter())
+                                .addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+                        .uri(httpUri)
+                        .order(0)
+                        .id("customer_filter_router"))*/
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+
+
+    @Bean
+    public RequestTimeGatewayFilterFactory elapsedGatewayFilterFactory() {
+        return new RequestTimeGatewayFilterFactory();
+    }
+
+
+
+
+}
